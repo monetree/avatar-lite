cac9b812181912ed0a478afccdd7de1c996add29
soubhagya <soubhagya@potential.ai>
2024-06-14 11:10:30 +0530

    added code

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,8 +15,8 @@ const Home = () =>  {
           companion
         </p>
         <div className="buttons" style={{marginTop: '50px'}}>
-          <a href="#" style={{color: '#333333', fontWeight: 600, marginRight: '10px'}}>Privacy policy</a>
-          <a href="#" style={{color: '#333333', fontWeight: 600, marginLeft: '10px'}}>Terms &amp; conditions</a>
+          <a href="/privacy" target="_blank" style={{color: '#333333', fontWeight: 600, marginRight: '10px'}}>Privacy policy</a>
+          <a href="/terms" target="_blank" style={{color: '#333333', fontWeight: 600, marginLeft: '10px'}}>Terms &amp; conditions</a>
         </div>
         <div className="phone-mockup">
           <img src="https://media.istockphoto.com/id/1731402068/photo/smartphone-with-a-blank-screen-on-a-white-background.webp?b=1&s=170667a&w=0&k=20&c=1Us7OAr5Cp51c0t6GmJG1jJC44ROcnu6JPWd4VEr9EE=" alt="Phone Mockup" />
